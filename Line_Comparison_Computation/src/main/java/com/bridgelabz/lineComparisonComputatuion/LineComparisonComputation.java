63f94da8e1e0894f8b6f3ed99fd68a142256625b
git-ashindra01 <ashindrapragyan.dev@gmail.com>
2023-02-06 02:33:02 +0530

    MODIFIED : Code modified to find Length of Line

--- Line_Comparison_Computation/src/main/java/com/bridgelabz/lineComparisonComputatuion/LineComparisonComputation.java
+++ Line_Comparison_Computation/src/main/java/com/bridgelabz/lineComparisonComputatuion/LineComparisonComputation.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.lineComparisonComputatuion;
 
+import java.lang.Math;
+
 public class LineComparisonComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+        int x1 = 0, y1 = 0, x2 = 3, y2 = 4;
+        double length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.println("Length of Line: " + length);
     }
 }
